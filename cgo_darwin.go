fc6396b1731d588cec36da49bd3457cb045eac3b
Zhu Hua <54zhua@gmail.com>
2026-06-01 13:51:22 +0800

    fix: remove unsupported $ORIGIN on macOS, keep ${SRCDIR} rpath only
    Go cgo security check rejects @executable_path in LDFLAGS.
    Remove $ORIGIN which dyld on macOS does not support anyway.
    Add ${SRCDIR}/../../../../lib/... rpath for vendor project-root layout.
    Keep ${SRCDIR}/third_party/... rpath for source-based development.

--- cgo_darwin.go
+++ cgo_darwin.go
@@ -5,9 +5,7 @@ package goface
 /*
 #cgo CFLAGS: -I${SRCDIR}/third_party/inspireface/include
 #cgo LDFLAGS: -L${SRCDIR}/../../../../lib/inspireface/lib/darwin_arm64 -L${SRCDIR}/third_party/inspireface/lib/darwin_arm64 -L/usr/local/lib -L/usr/lib -lInspireFace
-#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN
-#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN/lib/inspireface/lib/darwin_arm64
-#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN/lib
+#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/../../../../lib/inspireface/lib/darwin_arm64
 #cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/third_party/inspireface/lib/darwin_arm64
 */
 import "C"
